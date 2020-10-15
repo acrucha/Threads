33c415cc9f7827b67409edb70ad1d8e9954a5e0a
mluisass <mlss@cin.ufpe.br>
2020-10-15 13:53:11 -0300

    q1 comentada

--- 1.lista/Q1.c
+++ 1.lista/Q1.c
@@ -3,52 +3,58 @@
 #include <stdlib.h>
 
 long contador = 0;
-int flag=0;
-pthread_mutex_t mymutex = PTHREAD_MUTEX_INITIALIZER; //mutex estático
+int flag = 0; //determina se o contador chegou ou não a 1000000
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; //mutex estático
 
-void *inc(void *threadid)
+void *inc(void *threadid) //funcao de incrementar, recebe id da thread como parâmetro
 {
-    while(flag==0){
-        pthread_mutex_lock(&mymutex);
-        if(contador<1000000){
+    while (flag == 0)
+    {
+        pthread_mutex_lock(&mutex); //impede que outras threads utilizem o contador ao mesmo tempo
+        if (contador < 1000000)
+        {
             contador++;
-            if(contador==1000000){
+            if (contador == 1000000)
+            {
                 int tid = *((int *)threadid);
-                printf("o valor é %ld \n",contador);
-                printf("a thread é %d \n",tid);
-                flag=1;
+                printf("o valor é %ld \n", contador);
+                printf("a thread é %d \n", tid);
+                flag = 1; //determina que o contador chegou a 1000000, impedindo que ele seja incrementado novamente 
             }
         }
-        pthread_mutex_unlock(&mymutex);
+        pthread_mutex_unlock(&mutex); //libera o acesso ao contador para as demais threads
     }
-    pthread_exit(NULL);
+    pthread_exit(NULL); //encerra a thread
 }
-int main(/*int argc, char *argv[]*/)
+int main()
 {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n); //recebe a quantidade de threads a serem criadas do usuario
     pthread_t thread[n];
-    int *id[n];
+    int *id[n]; //vetor para guardar o id de cada thread
     int t;
     int rc;
-    for (t = 0; t < n; t++) //rodo o array
+    for (t = 0; t < n; t++)
     {
-        id[t] = (int *)malloc(sizeof(int)); //pra cada posição, eu aloco
-        *id[t] = t; //e digo q o valor é igual a esse número
-        printf("criando %d\n",*id[t]);
-        rc=pthread_create(&thread[t], NULL, inc, (void *)id[t]); //aqui o argumento da função é justamente aquele valr q eu dei
-        if(rc){
+        id[t] = (int *)malloc(sizeof(int));
+        *id[t] = t;                         
+        printf("criando %d\n", *id[t]);
+        rc = pthread_create(&thread[t], NULL, inc, (void *)id[t]);
+        if (rc)
+        {
             printf("ERRO; codigo de retorno eh %d\n", rc);
             exit(-1);
         }
     }
     int k;
-    for(k=0;k<n;k++){
-        pthread_join(thread[k],NULL);
+    for (k = 0; k < n; k++)
+    {
+        pthread_join(thread[k], NULL); //faz com que a main aguarde a conclusao de todas threads
     }
     pthread_exit(NULL);
     int h;
-    for(h=0;h<n;h++){
+    for (h = 0; h < n; h++)
+    {
         free(id[h]);
     }
 }
