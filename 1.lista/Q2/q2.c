0ff37f6da3158a367c96050a1e02afb0ad176310
mluisass <mlss@cin.ufpe.br>
2020-10-21 23:27:14 -0300

    dando merda só na leitura da 2 viagem

--- 1.lista/Q2/q2.c
+++ 1.lista/Q2/q2.c
@@ -2,12 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-1. recebe do usuario todos os dados
-2. criar uma matriz[n][] com o nome de cada arquivo (receber)
-3. criar 
-*/
-
 typedef struct Matriz
 {
     char linha; //numero da linha
@@ -19,8 +13,6 @@ void lerArquivos(int n, char *nomesArq[]);
 
 int main()
 {
-    //tem que ler tudo do usuário
-    //threads, linhas e arquivos
     int n; //numero de arquivos
     int t; //numero de threads (tem que ser <= arquivos)
     int l; //numeros de linhas
@@ -36,13 +28,10 @@ int main()
         if (t <= n)
             break;
     }
-    //se isso nao funcionar
-    char *nomesArq[n]; //vetor de nomes dos arquivos
-
+    char *nomesArq[n]; //vetor de nomes dos arquivos (também recebe do usuário?)
     *(nomesArq + 0) = "a.txt";
     *(nomesArq + 1) = "b.txt";
     lerArquivos(n, nomesArq);
-
     pthread_t thread[t]; //declaro as threads
     int i;
 
@@ -89,8 +78,10 @@ void criouThread(int rc)
 void lerArquivos(int n, char *nomesArq[])
 { //recebe a matriz com nome de todos arquivos
     FILE *arquivos[n];
-    int i, qtd;
-    Matriz *viagensInfo[n]; //vetor com todas as viagens --> VETOR DE PONTEIROS, linhas = qtd de arquivos, colunas = qtd de viagens de cada arquivo
+    int qtdViagens; //quantas viagens de trem tem em cada arquivo
+    int qtdBytes;   //quantos bytes tem em cada arquivo
+    int i;
+    Matriz *viagensInfo[n]; //matriz com todas as viagens, linhas = qtd de arquivos, colunas = qtd de viagens de cada arquivo
     for (i = 0; i < n; i++)
     {
         arquivos[i] = fopen(nomesArq[i], "r"); //abre arquivo com aquele nome
@@ -103,32 +94,26 @@ void lerArquivos(int n, char *nomesArq[])
         {
             printf("Abri arquivo %s\n", nomesArq[i]);
         }
-        fseek(arquivos[i], 0, SEEK_END);   //move cursor pro fim do arquivo
-        int qtdBytes = ftell(arquivos[i]); //conta quantos bytes tem até o fim do arquivo
-        // printf("qtd bytes = %d\n", qtdBytes);
-        // printf("sizeof matriz = %ld\n", sizeof(Matriz));
-        qtd = qtdBytes / sizeof(Matriz); //quantidade de viagens
-        // printf("qtd viagens = %d\n", qtd);
-        viagensInfo[i] = (Matriz *)malloc((qtd + 1) * sizeof(Matriz)); //alocando memória para o vetor de viagens
-
+        fseek(arquivos[i], 0, SEEK_END);                                      //move cursor pro fim do arquivo
+        qtdBytes = ftell(arquivos[i]);                                        //conta quantos bytes tem até o fim do arquivo
+        qtdViagens = qtdBytes / sizeof(Matriz);                               //quantidade de viagens
+        viagensInfo[i] = (Matriz *)malloc((qtdViagens + 1) * sizeof(Matriz)); //alocando memória para o vetor de viagens
         if (viagensInfo[i] == NULL)
             exit(-1);
-
         rewind(arquivos[i]); //voltando cursor para o começo do arquivo
-        int k;
-        while(fread(viagensInfo[i], sizeof(Matriz), qtd, arquivos[i])){
-            printf("%c\n %s\n",viagensInfo[i]->linha,viagensInfo[i]->texto);
-        }
-        /* while ((k < qtd) && (!feof(arquivos[i])))
+        int j = 0;
+        char branco;
+        for (j = 0; j < qtdViagens; j++)
         {
-            fscanf(arquivos[i], "%c %[^\n]", &viagensInfo[i][k].linha, viagensInfo[i][k].texto);
-            ++k;
-        }*/
-        // for(k = 0; k < qtd+1; k++){
-        //     fread(viagensInfo[i][k], sizeof(Matriz), 1, arquivos[i]); //salva em viagensInfo
-        // }
-        // printf("Arquivo %d\n\n", i + 1);
-       printf("%s\n", viagensInfo[i][k].texto);
+            fscanf(arquivos[i], " %c", &viagensInfo[i][j].linha);
+            fscanf(arquivos[i], " %c", &branco);
+            fscanf(arquivos[i], " %[^\n]", viagensInfo[i][j].texto);
+        }
+        int k = 0;
+        for (k = 0; k < qtdViagens; k++)
+        {
+            printf("linha %c\ntexto:%s\n", viagensInfo[i][k].linha, viagensInfo[i][k].texto);
+        }
     }
     /*int j;
     for(j = 0; j < qtd; j++){
@@ -152,4 +137,13 @@ for(k = 0; k < qtd+1; k++){
             fread(&(viagensInfo[i][k].linha), 1, 1, arquivos[i]); //salva em viagensInfo
             fread(viagensInfo[i][k].texto, 25, 1, arquivos[i]); //salva em viagensInfo
         }
-*/
+
+/* while ((k < qtd) && (!feof(arquivos[i])))
+        {
+            fscanf(arquivos[i], "%c\n%[^\n]", &viagensInfo[i][k].linha, viagensInfo[i][k].texto);
+            ++k;
+        }*/
+        // for(k = 0; k < qtd+1; k++){
+        //     fread(viagensInfo[i][k], sizeof(Matriz), 1, arquivos[i]); //salva em viagensInfo
+        // }
+        // printf("Arquivo %d\n\n", i + 1);
