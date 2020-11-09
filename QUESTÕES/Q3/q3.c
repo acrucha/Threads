c8773ab8ca1a9dd7d9a1a0691974f79d857eca20
Mileto <mileto@pop-os.localdomain>
2020-11-09 10:28:29 -0300

    !

--- QUESTÕES/Q3/q3.c
+++ QUESTÕES/Q3/q3.c
@@ -14,21 +14,21 @@ void* search(void *argumento){ //procura s2 dentro de s1
     int tamanho2 = strlen(s2);
     int i,k,j,flag;
     //pthread_mutex_lock(&mutex);
-    for(i=0+valor;i<tamanho1;i=i+p){ // esse for eh p procurar a s2
+    for(i=0+valor;i<tamanho1;i=i+p){ //o i começa pelo id de cada thread e varia de acordo com a quantidade de threads
         flag=0;
         k=i;
-        for(j=0;j<tamanho2;j++){
-            if(s2[j]==s1[k]){
-                k++;
+        for(j=0;j<tamanho2;j++){//roda um for com o tamanho da string 2
+            if(s2[j]==s1[k]){//verifica se a partir da posição k da string 1 vai ser igual a string 2 
+                k++;//varia o k para mudar a posição para a próxima comparação
             }
-            else{
+            else{//caso em algum momento da execução do for o if não for executado, ele termina a validação para aquela thead e da um break.
                 flag=1;
                 break;
             }
         }
-        if(flag==0){
+        if(flag==0){//caso ele não mude a flag( ele encontrou a string 2 na string 1)
             pthread_mutex_lock(&mutex);
-            contador++;
+            contador++;//aumenta o contador em 1 para cada ve que isso ocorre.
             pthread_mutex_unlock(&mutex);
         }
     }
@@ -42,22 +42,22 @@ int main(){
     scanf(" %[^\n]",s2);
     n1 = strlen(s1);
     n2 = strlen(s2);
-    for(i=n1;i>0;i--){   //rever isso depois c as meninas
-        if(n1%i == 0 && n2 < n1/i){
+    for(i=n1;i>0;i--){   //Implelemtado as condições impostas para o número p
+        if(n1%i == 0 && n2 < n1/i){// escolhe o maior npumero possível para p, vito que, o for é reverso.
             p=i;
             break;
         }
     }
-    int x[p]; 
+    int x[p];//vetor contendo o id de cada thread
     for (int a = 0; a < p; a++){
         x[a] = a;
     } 
     int t;
     int status;
     pthread_t thread[p];
-    for (t = 0; t < p; t++){
+    for (t = 0; t < p; t++){//cria as threads 
         status = pthread_create(&thread[t], NULL, search, (void *)&x[t]);
-        if (status){
+        if (status){//verifica se foi inicislizada
             printf("ERRO; codigo de retorno eh %d\n", status);
             exit(-1);
         }
